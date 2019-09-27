1bcd5d79fdf8ec278f08ce9ccbb141786247de66
Felix <felix.reisch@live.de>
2019-09-27 02:05:52 +0200

    Added gender message.

---
+++ src/TheFipster.Munchkin.GameDomain/Messages/GenderMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TheFipster.Munchkin.GameDomain.Messages
+{
+    public class GenderMessage : GameMessage
+    {
+        public Guid PlayerId { get; set; }
+        public string Gender { get; set; }
+    }
+}
