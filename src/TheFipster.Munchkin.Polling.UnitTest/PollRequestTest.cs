295816742b6b83f5e2522e819636d6f07ef2f8d8
Felix <felix.reisch@live.de>
2019-09-29 23:21:58 +0200

    Added some unit tests.

---
+++ src/TheFipster.Munchkin.Polling.UnitTest/PollRequestTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace TheFipster.Munchkin.Polling.UnitTest
+{
+    public class PollRequestTest
+    {
+        [Fact]
+        public void CreatePollRequest_ResultsInWaitStillRunning_Test()
+        {
+            var request = new PollRequest<string>();
+            var task = request.WaitAsync();
+
+            Assert.False(task.IsCompleted);
+        }
+
+        [Fact]
+        public void CreatePollRequestAndNotifyIt_ResultsInWaitTaskBeingCompleted_Test()
+        {
+            var expectedValue = "this is a message";
+            var request = new PollRequest<string>();
+            var task = request.WaitAsync();
+            request.Notify(expectedValue);
+
+            Assert.True(task.IsCompletedSuccessfully);
+        }
+    }
+}
---
+++ src/TheFipster.Munchkin.Polling.UnitTest/PollServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TheFipster.Munchkin.Polling.UnitTest
+{
+    public class PollServiceTest
+    {
+        private string someRequestId = "This is a request";
+
+        [Fact]
+        public void CheckUnknownRequest_ResultsInFalseReturn_Test()
+        {
+            // Arrange
+            var service = new PollService<string, Guid>(
+                1,
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromMilliseconds(100));
+
+            // Act & Assert
+            Assert.False(service.CheckRequest(someRequestId));
+        }
+
+        [Fact]
+        public void CheckExistingRequest_ResultsInTrueReturn_Test()
+        {
+            // Arrange
+            var service = new PollService<string, Guid>(
+                1,
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromMilliseconds(100));
+
+            // Act
+            service.StartRequest(someRequestId);
+
+            // Assert
+            Assert.True(service.CheckRequest(someRequestId));
+        }
+
+        [Fact]
+        public void CheckFinishedRequest_ResultsInFalseReturn_Test()
+        {
+            // Arrange
+            var service = new PollService<string, Guid>(
+                1,
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromMilliseconds(100));
+
+            // Act
+            service.StartRequest(someRequestId);
+            service.FinishRequest(someRequestId, Guid.NewGuid());
+
+            // Assert
+            Assert.False(service.CheckRequest(someRequestId));
+        }
+
+        [Fact]
+        public void StartingSameRequestTwice_ResultsInSameRequest_Test()
+        {
+            // Arrange
+            var service = new PollService<string, Guid>(
+                1,
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromMilliseconds(100));
+
+            // Act
+            var request1 = service.StartRequest(someRequestId);
+            var request2 = service.StartRequest(someRequestId);
+
+            // Assert
+            Assert.Equal(request1, request2);
+        }
+    }
+}
