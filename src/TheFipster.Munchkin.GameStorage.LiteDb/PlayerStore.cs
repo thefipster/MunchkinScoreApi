7616f7c5dbfde1456daa5d2573eb8cbb51e3c9e5
Felix.Reisch <Felix.Reisch@hc-vision.de>
2019-09-30 09:48:48 +0200

    Repaired litedb due to gitignore overlap.

---
+++ src/TheFipster.Munchkin.GameStorage.LiteDb/PlayerStore.cs
@@ -0,0 +1,30 @@
+﻿using LiteDB;
+using System;
+using System.Linq;
+using TheFipster.Munchkin.GameDomain;
+using TheFipster.Munchkin.GameDomain.Exceptions;
+
+namespace TheFipster.Munchkin.GameStorage.LiteDb
+{
+    public class PlayerStore : IPlayerStore
+    {
+        private readonly LiteCollection<GameMaster> _collection;
+
+        public PlayerStore(IRepository<GameMaster> repository)
+        {
+            var repo = repository;
+            _collection = repo.GetCollection();
+        }
+
+        public void Add(GameMaster gameMaster) =>
+            _collection.Upsert(gameMaster);
+
+        public GameMaster Get(string email) =>
+            _collection.Find(x => x.Email == email).FirstOrDefault() 
+            ?? throw new UnknownPlayerException();
+
+        public GameMaster Get(Guid playerId) =>
+            _collection.Find(x => x.Id == playerId).FirstOrDefault()
+            ?? throw new UnknownPlayerException();
+    }
+}
---
+++ src/TheFipster.Munchkin.GameStorage.LiteDb/Repository.cs
@@ -0,0 +1,17 @@
+﻿using LiteDB;
+
+namespace TheFipster.Munchkin.GameStorage.LiteDb
+{
+    public class Repository<T> : IRepository<T>
+    {
+        private const string Filename = "games.litedb";
+
+        private readonly LiteDatabase _db;
+
+        public Repository() =>
+            _db = new LiteDatabase(Filename);
+
+        public LiteCollection<T> GetCollection() =>
+            _db.GetCollection<T>();
+    }
+}
