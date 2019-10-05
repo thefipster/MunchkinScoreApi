0f0e58580adf95f3f4513c7af26aee7add494912
Felix <felix.reisch@live.de>
2019-10-05 22:58:55 +0200

    Added identity server template and added first api config.

---
+++ src/TheFipster.Munchkin.AuthApi/Config.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace TheFipster.Munchkin.AuthApi
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] {
+                new ApiResource("game-api", "Munchking Game API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] {
+                new Client
+                {
+                    AllowedScopes = { "game-api" },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientId = "client",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+        }
+    }
+}
