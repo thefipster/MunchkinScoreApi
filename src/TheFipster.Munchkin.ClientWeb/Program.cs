f161bfd1660a9a9d8c9e14dd9e10baeaa06de289
Felix <felix.reisch@live.de>
2019-10-07 00:25:53 +0200

    Added serilog to web client.

--- src/TheFipster.Munchkin.ClientWeb/Program.cs
+++ src/TheFipster.Munchkin.ClientWeb/Program.cs
@@ -1,26 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System.IO;
 
 namespace TheFipster.Munchkin.ClientWeb
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static void Main(string[] args) => WebHost
+            .CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+                config
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args))
+            .UseSerilog((hostingContext, loggerConfiguration) =>
+                loggerConfiguration
+                    .ReadFrom
+                    .Configuration(hostingContext.Configuration))
+            .UseStartup<Startup>()
+            .Build()
+            .Run();
     }
 }
