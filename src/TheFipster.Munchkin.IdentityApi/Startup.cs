c4755fcf13c976b933d33cfec1aae6d6f3675121
Felix <felix.reisch@live.de>
2019-10-12 23:27:49 +0200

    Added facebook, microsoft and twitter as external auth providers.

--- src/TheFipster.Munchkin.IdentityApi/Startup.cs
+++ src/TheFipster.Munchkin.IdentityApi/Startup.cs
@@ -28,55 +28,12 @@ namespace TheFipster.Munchkin.IdentityApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("IdentityConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddMvc(options => options.EnableEndpointRouting = false);
-
-            var corsOrigins = Clients
-                .Get()
-                .Where(x => x.AllowedCorsOrigins != null)
-                .SelectMany(x => x.AllowedCorsOrigins)
-                .ToArray();
-
-            services.AddCors(options =>
-            {
-                // this defines a CORS policy called "default"
-                options.AddPolicy("default", policy =>
-                {
-                    policy.WithOrigins(corsOrigins)
-                        .AllowAnyHeader()
-                        .AllowAnyMethod().AllowCredentials();
-                });
-            });
-
-            var builder = services.AddIdentityServer(options =>
-            {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
-            })
-                .AddInMemoryIdentityResources(Resources.Get())
-                .AddInMemoryApiResources(Apis.Get())
-                .AddInMemoryClients(Clients.Get())
-                .AddAspNetIdentity<ApplicationUser>();
-
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.ClientId = Configuration["ExternalProviders:Google:ClientId"];
-                    options.ClientSecret = Configuration["ExternalProviders:Google:ClientSecret"];
-                });
-
-            if (Environment.IsDevelopment())
-                builder.AddDeveloperSigningCredential();
-            else
-                throw new Exception("need to configure key material");
+            addIdentityStorage(services);
+            addMvc(services);
+            addCorsPolicy(services);
+            var identityServerBuilder = addIdentityServer(services);
+            addAuthenticationProviders(services);
+            addSigningCredentials(identityServerBuilder);
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
@@ -97,6 +54,90 @@ namespace TheFipster.Munchkin.IdentityApi
             app.UseStaticFiles();
             app.UseIdentityServer();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void addSigningCredentials(IIdentityServerBuilder identityServerBuilder)
+        {
+            if (Environment.IsDevelopment())
+                identityServerBuilder.AddDeveloperSigningCredential();
+            else
+                throw new Exception("need to configure key material");
+        }
+
+        private void addAuthenticationProviders(IServiceCollection services)
+        {
+            services
+                .AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = Configuration["ExternalProviders:Google:ClientId"];
+                    options.ClientSecret = Configuration["ExternalProviders:Google:ClientSecret"];
+                })
+                .AddFacebook(options =>
+                {
+                    options.ClientId = Configuration["ExternalProviders:Facebook:ClientId"];
+                    options.ClientSecret = Configuration["ExternalProviders:Facebook:ClientSecret"];
+                })
+                .AddMicrosoftAccount(options =>
+                {
+                    options.ClientId = Configuration["ExternalProviders:Microsoft:ClientId"];
+                    options.ClientSecret = Configuration["ExternalProviders:Microsoft:ClientSecret"];
+                })
+                .AddTwitter(options =>
+                {
+                    options.ConsumerKey = Configuration["ExternalProviders:Twitter:ConsumerKey"];
+                    options.ConsumerSecret = Configuration["ExternalProviders:Twitter:ConsumerSecret"];
+                });
+        }
+
+        private static IIdentityServerBuilder addIdentityServer(IServiceCollection services)
+        {
+            return services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                })
+                .AddInMemoryIdentityResources(Resources.Get())
+                .AddInMemoryApiResources(Apis.Get())
+                .AddInMemoryClients(Clients.Get())
+                .AddAspNetIdentity<ApplicationUser>();
+        }
+
+        private static void addCorsPolicy(IServiceCollection services)
+        {
+            var corsOrigins = Clients
+                .Get()
+                .Where(x => x.AllowedCorsOrigins != null)
+                .SelectMany(x => x.AllowedCorsOrigins)
+                .ToArray();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins(corsOrigins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod().AllowCredentials();
+                });
+            });
+        }
+
+        private static void addMvc(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        private void addIdentityStorage(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("IdentityConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
     }
 }
